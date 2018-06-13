a23724b6fb5cfaafddbe9cf668108684bc54f33b
Rahul Janghel <rjangheldotcom@gmail.com>
2018-06-12 22:55:56 -0700

    Adding SSH Module.

--- manifests/init.pp
+++ manifests/init.pp
@@ -46,6 +46,18 @@ class ssh (
   String $package_name = $::ssh::params::package_name,
   String $service_name = $::ssh::params::service_name,
 ) inherits ::ssh::params {
+  class { '::ssh::service': }
+  class { '::ssh::config': }
   class { 'ssh::install': } -> 
-  class { 'ssh::service': }
+
+  Class['::ssh::install']
+  -> Class['::ssh::config']
+  ~> Class[':ssh::service']
+  -> Class['ssh']
 }
+
+
+
+
+
+
